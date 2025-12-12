b9bb9d9ff8c26735f8d3bfa1bfc0a99f3df6532f
gv-ORC <108883488+gv-ORC@users.noreply.github.com>
2025-12-12 11:06:14 -0500

    New method of applying drift, simplifies logic and forces the limits to be phase-locked to the input more consistently.

--- rtl/generation.sv
+++ rtl/generation.sv
@@ -32,7 +32,7 @@ module generation (
 );
 
 /*
-
+! Cascade Drift
 ? Mimic Not 50-50: Low Half-Rate 2(h), High Half-Rate 2(l), Starts at (s) = 10, Rx Delay (r) = 3, Tx Delay (t) = 3, Counter(c), Drift Window (w) = 1
 * Incoming - The clock signal from the pin after it has been properly syncronized and had events extracted
 * Expected - Generated clock that mimics the cycle that the clock would have arrived directly at the Rx Pin
@@ -60,6 +60,37 @@ Preemptive Half-Rate Limit: x x x x x x x x x x x  -  -  -  16 16 16 18 18 20 20
 Preemptive High Half-Rate:  x x x x x x x x x x x  -  -  -  -  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2
 Preemptive Low Half-Rate:   x - - - - - - - - - -  -  -  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2
 
+
+! Insta-Sync Negative Drift
+? Mimic Not 50-50: Low Half-Rate 2(h), High Half-Rate 2(l), Starts at (s) = 10, Rx Delay (r) = 3, Tx Delay (t) = 3, Counter(c), Drift Window (w) = 1
+* Incoming - The clock signal from the pin after it has been properly syncronized and had events extracted
+* Drift(d) - 
+*          > Seed Drift by: [@f1] <= c + h + l
+* Expected(e) - Generated clock that mimics the cycle that the clock would have arrived directly at the Rx Pin
+*             > Seed Expected by: [@f1]+1 <= (d + h + l) - r
+* Premptive - Generated clock that triggers events earlier than the pin, to account for pipeline and sync latency
+*           > Seed Preemptive by: [@f1]+1 <= e - t
+
+> 
+                                                                                                       Neg Drift                    Neg Drift
+Incoming Edge Name:                                f0    r0    f1    r1    f2    r2    f3    r3    f4    r4(f5)   r5    f6    r6    f7(r7)    f8    r8    f9    r9    f10
+Incoming Clock:               xxxxxxx---------------______------______------______------______------______---______------______------___------______------______------___
+Counter(c):                   0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51
+Drift Edge Name:                                                           f2    r2    f3    r3    f4    r4(f5)    r5    f6    r6    f7 
+Drift Clock (fake):           xxxxxxxxxxxxxxxxxxxxxx------------------------______------______------______---______------______------___
+Drift High Half-Rate:         x x x x x x x x x x x  -  -  -  -  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2
+Drift Low Half-Rate:          x - - - - - - - - - -  -  -  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2  2
+Drift Event Upper Limit:      x x x x x x x x x x x  -  -  -  -  17 17 17 17 19 19 21 21 23 23 25 25 27 27 29 32 32 34 34 36 36 38 38 40
+Drift Target:                 x x x x x x x x x x x  -  -  -  -  18 18 18 18 20 20 22 22 24 24 26 26 28 28 30 31 31 33 33 35 35 37 37 39
+Drift Event Lower Limit:      x x x x x x x x x x x  -  -  -  -  19 19 19 19 21 21 23 23 25 25 27 27 29 29 31 30 30 32 32 34 34 36 36 38
+Drift:Expected Limit Delta:   x x x x x x x x x x x  -  -  -  -  -  1  1  1  -1 1  -1 1  -1 1  -1 1  -1 1  -1 -1 1  -1 1  -1 1  -1 1  -1
+Drift:Preemptive Limit Delta: x x x x x x x x x x x  -  -  -  -  -  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+Expected Edge Name:                                                           f3    r3    f4    r4    f5    r5(f6)    r6    f7    r7   
+Expected Clock:               xxxxxxxxxxxxxxxxxxxxxx---------------------------______------______------______---______------______------
+Expected Half-Rate Limit:     x x x x x x x x x x x  -  -  -  -  -  19 19 19 19 21 21 23 23 25 25 27 27 29 29 30 32 32 34 34 36 36 38 38
+Preemptive Edge Name:                                                f3    r3    f4    r4    f5    r5    f6(r6)   f7    r7    f8    r8  
+Preemptive Clock:             xxxxxxxxxxxxxxxxxxxxxx------------------______------______------______------___------______------______---
+Preemptive Half-Rate Limit:   x x x x x x x x x x x  -  -  -  -  -  16 18 18 20 20 22 22 24 24 26 26 28 28 30 31 31 33 33 35 35 37 37 39
 */
 
 // Counter

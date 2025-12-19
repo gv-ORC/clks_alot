a37a633cb5987303b012eee5bf65f688f9a5eaa8
gv-ORC <g.viscardi@orchub.net>
2025-12-19 10:42:16 -0500

    I now see my verilator warnings :D

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# Must run `nix develop` anytime this is changed before you can run `fusesoc *` using any saved changes.
 {
     description = "Digital PLL";
 
@@ -5,7 +6,13 @@
         nixpkgs.url = "nixpkgs/nixos-25.11";
         flake-parts.url = "github:hercules-ci/flake-parts";
         fusesoc-flake.url = "github:Mop-u/fusesoc-flake";
+
+        softlibs = {
+            url = "github:kara-partners/softlibs/0.1";
+            flake = false;
+            };
         };
+
     outputs =
         inputs@{ flake-parts, ... }:
         flake-parts.lib.mkFlake { inherit inputs; } {
@@ -24,15 +31,18 @@
                     fusesoc-project = {
                         withVerilator = true;
                         withCcache = true;
-                        sources.local = "src";
+                        sources = {
+                            inherit (inputs) softlibs;
+                            local = "rtl";
+                        };
                         extraPackages = [
                             pkgs.verible
                             pkgs.haskellPackages.sv2v
-                            (inputs'.quartus.packages.mkVersion {
-                                version = 24;
-                                edition = "pro";
-                                extraArgs.devices = [ "cyclone10gx" ];
-                            })
+                            # (inputs'.quartus.packages.mkVersion {
+                            #     version = 24;
+                            #     edition = "pro";
+                            #     extraArgs.devices = [ "cyclone10gx" ];
+                            # })
                         ];
                     };
                 };
